b029d97ea499ae516b2a8e09e86380e15033ec01
Anastasiia Balieieva <baleevaanastasiz@gmail.com>
2026-08-12 19:48:00 +0200

    feat: header component

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen max-w-7xl mx-auto px-10">
+    <NavBar />
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
