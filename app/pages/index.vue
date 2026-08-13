e5f202c271522665cfa5868b1cd77737bb552286
Anastasiia Balieieva <baleevaanastasiz@gmail.com>
2026-08-13 12:38:12 +0200

    fix: reset hash when reloaded

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,18 @@
   <div>
     <HeroSection />
     <ServicesSection />
+    <ExperienceSection />
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const router = useRouter()
+onMounted(() => {
+  if (window.location.hash) {
+    router.replace({ hash: '' })
+    window.scrollTo({ top: 0, behavior: 'auto' })
+  }
+})
+</script>
 
 <style scoped></style>
