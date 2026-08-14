0b2521a603d12bcc28f2bc0fed77dc6c33c3cd0d
Anastasiia Balieieva <baleevaanastasiz@gmail.com>
2026-08-14 16:04:50 +0200

    fix: move contact form to default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,6 +3,7 @@
     <div class="flex flex-col min-h-screen">
       <div class="w-full max-w-7xl mx-auto px-10 grow">
         <NavBar />
+        <ContactForm v-if="showcontact" />
         <main>
           <slot></slot>
         </main>
@@ -11,3 +12,7 @@
     </div>
   </BgInteractive>
 </template>
+
+<script lang="ts" setup>
+const showcontact = useState<boolean>('showcontact', () => false)
+</script>
